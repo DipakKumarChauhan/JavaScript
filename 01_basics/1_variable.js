ada98c38de305928c74be89f8ffa19775230ec7d
DipakKumarChauhan <ece22133@iiitkalyani.ac.in>
2025-01-09 20:13:04 +0530

    Initial Commitcontaining var and datatypes notes

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,9 @@
+const accountId = 144553 //constant variable value
+
+let accountEmail = "dipak@gmail.com" // this value can be changed
+var accPass = "12345" // scope me js confused rehta in block scope and functional scope hai so use var
+accountCity = "Kolkata"
+let accState // variable without initial value
+console.log(accountId) //used to print a variable
+
+console.table([accountId, accountEmail, accPass, accountCity, accState])
---
+++ 01_basics/2_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) -> doesnt work in node but in browser to print 
+
+/*
+datatypes are -> number ,  bigint(for big numbers) , string "" , boolean , null (stand-alone value)
+undefined ( means no value assigned) , symbol (used to find uniqueness), object
+*/
+let age = 22;
+console.log(typeof(age))
+console.log(typeof(null)) // it will give object 
+console.log(typeof undefined)

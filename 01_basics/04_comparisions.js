d8b0f551926dbe72a5bd5eef2f8e785f6f008ef3
DipakKumarChauhan <ece22133@iiitkalyani.ac.in>
2025-01-09 22:27:10 +0530

    comparisions added

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,23 @@
+// only adding which we dont know
+
+// console.log("2" + 1)
+// console.log("02" + 1)
+
+// we must not compare two different data types
+
+// Avoid these codes.
+
+// console.log(null > 0) //false
+// console.log(null == 0) //false
+// console.log(null < 0) //false
+
+// console.log(null > 0) //false
+// console.log(null == 0) //false
+// console.log(null >= 0) //true here it converts null to a number
+
+// console.log(undefined > 0) //false
+// console.log(undefined == 0) //false
+// console.log(undefined >= 0)  // false
+
+// === this does strict check and does no conversion
+console.log("2" ===2) ; // false

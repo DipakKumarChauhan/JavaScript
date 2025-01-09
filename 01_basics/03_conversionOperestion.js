9e04b820f259d3cf2b3fed915025267f2d04baf7
DipakKumarChauhan <ece22133@iiitkalyani.ac.in>
2025-01-09 22:17:01 +0530

    operations added

---
+++ 01_basics/03_conversionOperestion.js
@@ -0,0 +1,85 @@
+// let score  = "33"
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+
+// let score2 = "33abc"
+// console.log(typeof(score2))
+
+// let valueInNumber2 = Number(score2)
+// console.log(typeof(valueInNumber2))
+// console.log(valueInNumber2); 
+// // this give Nan meaning conversion sahi se nhai hua
+
+// let score3 = null
+// console.log(typeof(score3))
+
+// let valueInNumber3 = Number(score3)
+// console.log(typeof(valueInNumber3))
+// console.log(valueInNumber3); 
+// // this gives zero
+
+// let score4 = undefined
+// console.log(typeof(score4))
+
+// let valueInNumber4 = Number(score4)
+// console.log(typeof(valueInNumber4))
+// console.log(valueInNumber4); 
+// //this gives Nan
+
+// let score5 = true
+// console.log(typeof(score5))
+
+// let valueInNumber5 = Number(score5)
+// console.log(typeof(valueInNumber5))
+// console.log(valueInNumber5); 
+
+// let score6 = "Hitesh"
+// console.log(typeof(score6))
+
+// let valueInNumber6 = Number(score6)
+// console.log(typeof(valueInNumber6))
+// console.log(valueInNumber6); 
+// //This gives NaN
+
+//#############################
+// to conver in bool and others
+
+// let logIn = 1
+// let boolLog = Boolean(logIn)
+// console.log(boolLog)
+
+// let logIn2 = ""
+// let boolLog2 = Boolean(logIn2)
+// console.log(boolLog2) 
+// // false
+
+// let logIn3 = "hitesh"
+// let boolLog3 = Boolean(logIn3)
+// console.log(boolLog3)
+//  // this gives true.
+
+//#########################
+// To conver into string
+
+// let someNum = 33
+// let stringNum = String(someNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+// #########################################
+//-----------------------x---Operations
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue) 
+
+// -3
+
+// 2**3 is 2^3
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");

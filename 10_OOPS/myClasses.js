8eaceba710b1c606780cc9d6dccbcc441ec90906
DipakKumarChauhan <ece22133@iiitkalyani.ac.in>
2025-01-25 02:09:05 +0530

    OOPs inheretence  notes added

--- 10_OOPS/myClasses.js
+++ 10_OOPS/myClasses.js
@@ -21,5 +21,24 @@ const chai = new User("chai" , "chais@gmail.com" , "123")
 console.log(chai.encryptPassword());
 console.log(chai.changeUserName());
 
+// Behid Scene
 
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
 

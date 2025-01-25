2c368c70da53449b41111435ed0c4ed60c7d078b
DipakKumarChauhan <ece22133@iiitkalyani.ac.in>
2025-01-25 22:08:29 +0530

    Getter And Setter Notes added

--- 10_OOPS/object_get_set.js
+++ 10_OOPS/object_get_set.js
@@ -2,7 +2,7 @@ const User = {
     _email: 'h@hc.com',
     _password: "abc",
 
-
+// in _email -> " _ " means a private property is defined
     get email(){
         return this._email.toUpperCase()
     },
@@ -12,5 +12,11 @@ const User = {
     }
 }
 
+// Though this method is weird but  here we can use factory function and access them directly
 const tea = Object.create(User)
 console.log(tea.email);
+
+// So get generally overwrites the property of the object so even if we use tea.email we can see value printed coz
+// get and set simply overwrites the property 
+
+//For user Get and set methods are actually a propety only
